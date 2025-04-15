831978a79883690783088d5264b6db0f8a64b859
YuushaExa <61891530+YuushaExa@users.noreply.github.com>
2025-04-16 00:42:26 +0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,106 @@
+// data.js
+export const heroes = [
+    {
+        id: 1,
+        name: "Warrior",
+        attack: 10,
+        defense: 8,
+        health: 100,
+        speed: 5,
+        color: "#FF0000", // Red
+    },
+    {
+        id: 2,
+        name: "Mage",
+        attack: 15,
+        defense: 3,
+        health: 70,
+        speed: 7,
+        color: "#0000FF", // Blue
+    },
+    {
+        id: 3,
+        name: "Ranger",
+        attack: 12,
+        defense: 5,
+        health: 80,
+        speed: 10,
+        color: "#00FF00", // Green
+    },
+    {
+        id: 4,
+        name: "Paladin",
+        attack: 8,
+        defense: 12,
+        health: 120,
+        speed: 4,
+        color: "#FFFF00", // Yellow
+    },
+    {
+        id: 5,
+        name: "Rogue",
+        attack: 14,
+        defense: 6,
+        health: 75,
+        speed: 12,
+        color: "#800080", // Purple
+    }
+];
+
+export const monsters = [
+    {
+        id: 1,
+        name: "Goblin",
+        attack: 5,
+        defense: 2,
+        health: 30,
+        speed: 6,
+        color: "#00AA00", // Dark green
+        gold: 5,
+        xp: 10
+    },
+    {
+        id: 2,
+        name: "Skeleton",
+        attack: 7,
+        defense: 3,
+        health: 40,
+        speed: 4,
+        color: "#DDDDDD", // Light gray
+        gold: 8,
+        xp: 15
+    },
+    {
+        id: 3,
+        name: "Orc",
+        attack: 10,
+        defense: 5,
+        health: 60,
+        speed: 5,
+        color: "#AA5500", // Brown
+        gold: 12,
+        xp: 20
+    },
+    {
+        id: 4,
+        name: "Slime",
+        attack: 3,
+        defense: 1,
+        health: 50,
+        speed: 3,
+        color: "#00AAAA", // Teal
+        gold: 3,
+        xp: 5
+    },
+    {
+        id: 5,
+        name: "Wolf",
+        attack: 8,
+        defense: 4,
+        health: 45,
+        speed: 8,
+        color: "#666666", // Gray
+        gold: 7,
+        xp: 12
+    }
+];
